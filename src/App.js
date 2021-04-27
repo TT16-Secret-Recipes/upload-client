204e3dfe3919cb814806724ed179bc9cb565bcc0
Max Huckstepp <38843399+mhuckstepp@users.noreply.github.com>
2021-04-27 15:24:46 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
-  const handleSubmit = e => {
+  const [file, setFile] = useState({});
+
+  const changeHandler = (e) => {
+    console.log(e.target.files[0]);
+    setFile(e.target.files[0]);
+  };
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(e)
-  }
+    console.log(file);
+    axios
+      .post("http://localhost:8000/upload", file, {
+        // receive two parameter endpoint url ,form data
+      })
+      .then((res) => {
+        // then print response status
+        console.log(res.statusText);
+      });
+  };
+
   return (
     <div className="app">
       <h1>Hello World!</h1>
-      <form id="upload" action="/profile" method="post" enctype="multipart/form-data" onSubmit={handleSubmit}>
-        <input type="file" name="avatar" />
-        <button class='btn-alert' type='submit'> Upload </button>
-
+      <form
+        id="upload"
+        action="/profile"
+        method="post"
+        enctype="multipart/form-data"
+        onSubmit={handleSubmit}
+      >
+        <input type="file" name="file" onChange={changeHandler} />
+        <button class="btn-alert" type="submit">
+          {" "}
+          Upload{" "}
+        </button>
       </form>
     </div>
   );
 }
 
 export default App;
+
