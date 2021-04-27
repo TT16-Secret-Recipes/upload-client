9a4ebc4de24a59d5a8e5e091ac4149714eb77f28
Garrick Suemith <garrick.suemith@gmail.com>
2021-04-27 18:08:31 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
 
 function App() {
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(e)
+  }
   return (
     <div className="app">
       <h1>Hello World!</h1>
+      <form id="upload" action="/profile" method="post" enctype="multipart/form-data" onSubmit={handleSubmit}>
+        <input type="file" name="avatar" />
+        <button class='btn-alert' type='submit'> Upload </button>
+
+      </form>
     </div>
   );
 }
